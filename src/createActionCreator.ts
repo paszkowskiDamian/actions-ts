daacd1c65a294186336094d6350deb2ce3626837
paszkowskiDamian <paszko.damian@gmail.com>
2018-06-10 22:59:20 +0200

    add licance

---
+++ src/createActionCreator.ts
@@ -0,0 +1,18 @@
+import { createAction } from './createAction';
+import { StandardAction } from './standardAction';
+import { ActionType } from './types';
+
+type ActionCreator<T extends ActionType, P = undefined, E = undefined, M = undefined> =
+  M extends undefined
+    ? E extends undefined
+      ? P extends undefined
+        ? (payload?: P, error?: E, meta?: M) => StandardAction<T,P,E,M>
+        : (payload: P, error?: E, meta?: M) => StandardAction<T,P,E,M>
+      : (payload: P, error: E, meta?: M) => StandardAction<T,P,E,M>
+    : (payload: P, error: E, meta: M) => StandardAction<T,P,E,M>
+
+export function createActionCreator<P = undefined, E = undefined, M = undefined>(type: ActionType): ActionCreator<ActionType,P,E,M>{
+  return (payload: P, error: E,meta: M) => createAction(type, payload, error, meta)
+}
+
+const a = createActionCreator<{id: string}>('HELLO')({id: 'ds'})
--- src/standardAction.ts
+++ src/standardAction.ts
@@ -1,24 +1,10 @@
-interface BaseAction<T>{
-    type: T;
-}
-
-interface Payload<P> {
-    payload: P;
-}
-
-interface Meta<M> {
-    meta: M;
-}
-
-interface ActionError<E> {
-    error: E;
-}
+import { Payload, Meta, ActionError, BaseAction, ActionType } from './types';
 
 type ActionWithPayload<A, P = undefined> = P extends undefined ? A : A & A & Payload<P>
 type ActionWithMeta<A, M = undefined> = M extends undefined ? A : A & Meta<M>
 type ActionWithError<A, E = undefined> = E extends undefined ? A : A & ActionError<E>
 
-export type StandardAction<T, P = undefined, E = undefined, M = undefined> =
+export type StandardAction<T extends ActionType, P = undefined, E = undefined, M = undefined> =
 ActionWithError<
     ActionWithMeta<
         ActionWithPayload<
@@ -27,14 +13,5 @@ ActionWithError<
     M>,
 E>
 
-export function createAction<T,P = undefined,E = undefined,M = undefined>
-(type: T, payload?: P, error?: E, meta?: M): StandardAction<T,P,E,M> {
-  const action = { type }
-  const actionWithPayload = payload !== undefined ? {...action, payload} : action
-  const actionWithError = error !==undefined ? {...actionWithPayload, error} : actionWithPayload
-  const actionWithMeta = meta !== undefined ? {...actionWithError, meta} : actionWithError
-
-  return actionWithMeta as StandardAction<T,P,E,M>
-}
 
 
