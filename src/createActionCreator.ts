fdcf69ecda5e95d01b43a6e8c7f9773a3e950b46
paszkowskiDamian <paszko.damian@gmail.com>
2018-06-11 10:59:26 +0200

    add page source to package.json

--- src/createActionCreator.ts
+++ src/createActionCreator.ts
@@ -1,18 +1,18 @@
 import { createAction } from './createAction';
-import { StandardAction } from './standardAction';
+// import { StandardAction } from './standardAction';
 import { ActionType } from './types';
 
-type ActionCreator<T extends ActionType, P = undefined, E = undefined, M = undefined> =
-  M extends undefined
-    ? E extends undefined
-      ? P extends undefined
-        ? (payload?: P, error?: E, meta?: M) => StandardAction<T,P,E,M>
-        : (payload: P, error?: E, meta?: M) => StandardAction<T,P,E,M>
-      : (payload: P, error: E, meta?: M) => StandardAction<T,P,E,M>
-    : (payload: P, error: E, meta: M) => StandardAction<T,P,E,M>
+// type ActionCreator<T extends ActionType, P = undefined, E = undefined, M = undefined> =
+//   M extends undefined
+//     ? E extends undefined
+//       ? P extends undefined
+//         ? (payload?: P, error?: E, meta?: M) => StandardAction<T,P,E,M>
+//         : (payload: P, error?: E, meta?: M) => StandardAction<T,P,E,M>
+//       : (payload: P, error: E, meta?: M) => StandardAction<T,P,E,M>
+//     : (payload: P, error: E, meta: M) => StandardAction<T,P,E,M>
 
-export function createActionCreator<P = undefined, E = undefined, M = undefined>(type: ActionType): ActionCreator<ActionType,P,E,M>{
+export function createActionCreator<P = undefined, E = undefined, M = undefined>(type: ActionType): any{
   return (payload: P, error: E,meta: M) => createAction(type, payload, error, meta)
 }
 
-const a = createActionCreator<{id: string}>('HELLO')({id: 'ds'})
+// const a = createActionCreator<{id: string}>('HELLO')({id: 'ds'})
